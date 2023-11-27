6f2e243b51b8af963cd3b77b7453a1fb75fab5ae
Hannes Ehringfeld <hannes.ehringfeld@rub.de>
2023-11-27 21:07:47 +0100

    added basic vae for mnist data

---
+++ VAE/basic_vae_mnist/README.md
@@ -0,0 +1,12 @@
+# Variational Autoencoder
+- First implementation based on [[this tutorial](https://medium.com/@rekalantar/variational-auto-encoder-vae-pytorch-tutorial-dce2d2fe0f5f)](https://medium.com/@rekalantar/variational-auto-encoder-vae-pytorch-tutorial-dce2d2fe0f5f)
+- I trained the variational autoencoder on the MNIST dataset for 50 epochs locally and was able to replicate the results from the tutorial:
+  - Epoch 50 	Average Loss:  133.10832231779528
+  - Overall loss: 7973188.5068359375
+  - My latent space visualizatio:
+    ![Latent space visualization](docs/latent_space_visualization.png)
+
+
+
+### Promising links:
+- https://github.com/AntixK/PyTorch-VAE
---
+++ VAE/basic_vae_mnist/train.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.optim import Adam
+import matplotlib.pyplot as plt
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from mpl_toolkits.axes_grid1 import ImageGrid
+from torchvision.utils import save_image, make_grid
+from vae import VAE
+
+# create a transoform to apply to each datapoint
+transform = transforms.Compose([transforms.ToTensor()])
+
+# download the MNIST datasets
+path = 'data'
+train_dataset = MNIST(path, transform=transform, download=True)
+test_dataset  = MNIST(path, transform=transform, download=True)
+
+# create train and test dataloaders
+batch_size = 100
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# get 25 sample training images for visualization
+dataiter = iter(train_loader)
+image = next(dataiter)
+
+num_samples = 25
+sample_images = [image[0][i,0] for i in range(num_samples)] 
+
+fig = plt.figure(figsize=(5, 5))
+grid = ImageGrid(fig, 111, nrows_ncols=(5, 5), axes_pad=0.1)
+
+for ax, im in zip(grid, sample_images):
+    ax.imshow(im, cmap='gray')
+    ax.axis('off')
+
+plt.show()
+
+### train the VAE
+
+model = VAE().to(device)
+optimizer = Adam(model.parameters(), lr=1e-3)
+
+def loss_function(x, x_hat, mean, log_var):
+    reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
+    KLD = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
+
+    return reproduction_loss + KLD
+
+def train(model, optimizer, epochs, device):
+    model.train()
+    for epoch in range(epochs):
+        overall_loss = 0
+        for batch_idx, (x, _) in enumerate(train_loader):
+            x_dim = x.shape[1]*x.shape[2]*x.shape[3]
+            x = x.view(batch_size, x_dim).to(device)
+
+            optimizer.zero_grad()
+
+            x_hat, mean, log_var = model(x)
+            loss = loss_function(x, x_hat, mean, log_var)
+            
+            overall_loss += loss.item()
+            
+            loss.backward()
+            optimizer.step()
+
+        print("\tEpoch", epoch + 1, "\tAverage Loss: ", overall_loss/(batch_idx*batch_size))
+    return overall_loss
+
+train(model, optimizer, epochs=50, device=device)
+
+# save the model
+torch.save(model.state_dict(), f'model.pth')
+
+
+def generate_digit(mean, var):
+    z_sample = torch.tensor([[mean, var]], dtype=torch.float).to(device)
+    x_decoded = model.decode(z_sample)
+    digit = x_decoded.detach().cpu().reshape(28, 28) # reshape vector to 2d array
+    plt.imshow(digit, cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+generate_digit(0.0, 1.0), generate_digit(1.0, 0.0)
+
+def plot_latent_space(model, scale=1.0, n=25, digit_size=28, figsize=15):
+    # display a n*n 2D manifold of digits
+    figure = np.zeros((digit_size * n, digit_size * n))
+
+    # construct a grid 
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = torch.tensor([[xi, yi]], dtype=torch.float).to(device)
+            x_decoded = model.decode(z_sample)
+            digit = x_decoded[0].detach().cpu().reshape(digit_size, digit_size)
+            figure[i * digit_size : (i + 1) * digit_size, j * digit_size : (j + 1) * digit_size,] = digit
+
+    plt.figure(figsize=(figsize, figsize))
+    plt.title('VAE Latent Space Visualization')
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("mean, z [0]")
+    plt.ylabel("var, z [1]")
+    plt.imshow(figure, cmap="Greys_r")
+    plt.show()
+
+
+plot_latent_space(model)
