4eb46d8e3632ab612ff8e9a0acd3751049d3f9ef
Hannes Ehringfeld <hannes.ehringfeld@rub.de>
2023-11-27 23:37:03 +0100

    added test function

--- VAE/basic_vae/train.py
+++ VAE/basic_vae/train.py
@@ -9,7 +9,8 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 from torchvision.utils import save_image, make_grid
 from vae import VAE
 from dataset import BioData
-from utils import visualize_sample
+from utils import visualize_sample, visualize_comparison
+from tqdm import tqdm
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -20,10 +21,18 @@ print("number of samples in dataset: ", len(bio_dataset))
 print("shape of first sample: ", bio_dataset[0].shape)
 # visualize_sample(bio_dataset[214], bio_dataset.min_val, bio_dataset.max_val)
 
+# split dataset into training and test set (80/20)
+train_size = int(0.8 * len(bio_dataset))
+test_size = len(bio_dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(bio_dataset, [train_size, test_size])
 
 ## create dataloader for training
 batch_size = 100
-train_loader = DataLoader(dataset=bio_dataset, batch_size=batch_size, shuffle=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+print("batch size: ", batch_size)
+print("number of batches in train loader: ", len(train_loader))
+print("number of batches in test loader: ", len(test_loader))
 
 
 ### train the VAE
@@ -33,10 +42,7 @@ optimizer = Adam(model.parameters(), lr=1e-3)
 def loss_function(x, x_hat, mean, log_var):
     reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
     KLD = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
-
     return reproduction_loss + KLD
-
-from tqdm import tqdm
 
 def train(model, optimizer, epochs, device, train_loader):
     model.train()
@@ -66,5 +72,48 @@ def train(model, optimizer, epochs, device, train_loader):
 
 train(model, optimizer, epochs=1, device=device, train_loader=train_loader)
 
-torch.save(model.state_dict(), f'model.pth')
+
+### test the VAE
+def test(model, device, test_loader):
+    model.eval()  # Set the model to evaluation mode
+    test_loss = 0
+    with torch.no_grad():  # No gradients needed for testing
+        for x in test_loader:
+            x = x.to(device)
+
+            # Forward pass through the model
+            x_hat, mean, log_var = model(x)
+            loss = loss_function(x, x_hat, mean, log_var)
+            
+            test_loss += loss.item()
+
+    # Compute the average loss
+    average_loss = test_loss / len(test_loader.dataset)
+    print("\tTest Set Average Loss: ", average_loss)
+    return average_loss
+
+average_loss = test(model, device, test_loader)
+
+torch.save(model.state_dict(), f'models/average_test_loss_{average_loss}.pth')
+
+
+### plot examples
+
+num_samples_to_visualize = 5
+min_val = bio_dataset.min_val
+max_val = bio_dataset.max_val
+model.eval()
+with torch.no_grad():
+    # Assuming test_loader is available and has batch_size of 1 for simplicity
+    for i, x in enumerate(test_loader):
+        if i >= num_samples_to_visualize:
+            break
+        x = x.to(device)
+        reconstructed_x, _, _ = model(x)
+        
+        # Reshape tensors if necessary to match the expected 1D format
+        x_flat = x.view(-1)
+        reconstructed_x_flat = reconstructed_x.view(-1)
+
+        visualize_comparison(x_flat, reconstructed_x_flat, min_val, max_val)
 
--- VAE/basic_vae/utils.py
+++ VAE/basic_vae/utils.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
 
-import matplotlib.pyplot as plt
-import torch
-
 def visualize_sample(tensor, min_val, max_val):
     # Ensure the tensor is on the CPU and convert it to numpy for plotting
     if tensor.device.type == 'cuda':
@@ -22,3 +19,30 @@ def visualize_sample(tensor, min_val, max_val):
     plt.legend()
     plt.grid(True)
     plt.show()
+
+
+def visualize_comparison(original_tensor, reconstructed_tensor, min_val, max_val):
+    # Ensure both tensors are on the CPU and convert them to numpy for plotting
+    if original_tensor.device.type == 'cuda':
+        original_tensor = original_tensor.cpu()
+    if reconstructed_tensor.device.type == 'cuda':
+        reconstructed_tensor = reconstructed_tensor.cpu()
+    
+    # Rescale both tensors back to their original scale
+    original_tensor = original_tensor * (max_val - min_val) + min_val
+    reconstructed_tensor = reconstructed_tensor * (max_val - min_val) + min_val
+
+    original_data = original_tensor.numpy()
+    reconstructed_data = reconstructed_tensor.numpy()
+
+    # Plotting
+    plt.figure(figsize=(10, 4))
+    plt.plot(original_data, label='Original Data', color='blue')
+    plt.plot(reconstructed_data, label='Reconstructed Data', color='red')
+    plt.xlabel('Channel')
+    plt.ylabel('Value')
+    plt.title('Comparison of Original and Reconstructed Data')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
